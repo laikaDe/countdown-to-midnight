d6e6cf41129f99749db270c2b8ada6db7f7ff30a
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-01-15 19:05:46 -0800

    commit tree f4f4c7024bc2071c6ee1a480febf1b198f1834cc

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    int = 0
+    while int < num
+        puts "#{num} SECOND(S)!"
+        num -= 1
+        if num == 0
+            break
+        end
+    end
+    return "HAPPY NEW YEAR!" 
+end
+
+def countdown_with_sleep(num)
+    int = 0
+    while int < num
+        puts "#{num} SECOND(S)!"
+        num -= 1
+        sleep 1
+        if num == 0
+            break
+        end
+    end
 end
